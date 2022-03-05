e9c85ce12401ca02fde3084b03182ab859d89729
Gabriel Borges <gbdesigns13@gmail.com>
2022-03-05 18:35:31 -0300

    Projeto inicial

---
+++ App.tsx
@@ -0,0 +1,48 @@
+import 'intl';
+import 'intl/locale-data/jsonp/pt-BR';
+
+import React from 'react';
+import { ThemeProvider } from 'styled-components/native';
+import theme from './src/global/styles/theme';
+
+import { Routes } from './src/routes';
+
+import { 
+  useFonts,
+  Epilogue_400Regular,
+  Epilogue_700Bold,
+  Epilogue_800ExtraBold,
+  Epilogue_500Medium,
+  Epilogue_600SemiBold
+} from '@expo-google-fonts/epilogue';
+
+import {
+  Rajdhani_700Bold,
+  Rajdhani_500Medium
+} from '@expo-google-fonts/rajdhani';
+
+import AppLoading from 'expo-app-loading';
+import { StatusBar } from 'react-native';
+
+export default function App() {
+  const [fontsLoaded] = useFonts({
+    Epilogue_400Regular,
+    Epilogue_700Bold,
+    Epilogue_800ExtraBold,
+    Epilogue_500Medium,
+    Epilogue_600SemiBold,
+    Rajdhani_700Bold,
+    Rajdhani_500Medium
+  });
+
+  if(!fontsLoaded) {
+    return <AppLoading />
+  }
+
+  return (
+    <ThemeProvider theme={theme}>
+      <StatusBar barStyle="light-content"/>
+      <Routes />
+    </ThemeProvider>
+  )
+}
---
+++ src/global/styles/theme.ts
@@ -0,0 +1,27 @@
+export default {
+  colors: {
+    primary: "#FF5100",
+    background: "#282828",
+    shape: "#323232",
+    textVeryLight: "#BFBFBF",
+    textLight: "#C6C6C6",
+    text: "#959595",
+    success: "#04D361",
+    error: "#DA3E3E",
+    white: "#fff",
+  },
+
+  fonts: {
+    primary: {
+      regular: "Epilogue_400Regular",
+      bold: "Epilogue_700Bold",
+      extraBold: "Epilogue_800ExtraBold",
+      medium: "Epilogue_500Medium",
+      semiBold: "Epilogue_600SemiBold",
+    },
+    secondary: {
+      bold: "Rajdhani_700Bold",
+      medium: "Rajdhani_500Medium",
+    },
+  },
+};
